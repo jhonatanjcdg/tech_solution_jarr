6ac228e5a9c2db858114f6f00b682daabfd2e27a
jhonatanjcdg <j3172224877@gmail.com>
2026-04-15 07:25:04 -0500

    first commit -m

---
+++ src/main/java/com/tech_solution_jarr/tech_solution_jarr/TechSolutionJarrApplication.java
@@ -0,0 +1,13 @@
+package com.tech_solution_jarr.tech_solution_jarr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSolutionJarrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSolutionJarrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech_solution_jarr/tech_solution_jarr/TechSolutionJarrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_solution_jarr.tech_solution_jarr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSolutionJarrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
